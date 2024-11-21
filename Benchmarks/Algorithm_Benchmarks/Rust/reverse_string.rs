d4b24bdb786796057f97e55899b5ee9b4951d688
Yuchen Zhang <yzhang0701@gmail.com>
2024-11-21 15:14:22 -0500

    Update reverse_string.rs
    Mod: Fix indentation

--- Benchmarks/Algorithm_Benchmarks/Rust/reverse_string.rs
+++ Benchmarks/Algorithm_Benchmarks/Rust/reverse_string.rs
@@ -1,35 +1,31 @@
-pub fn reverse(chars: &mut Vec, len: usize) {
+pub fn reverse(chars: &mut Vec<_>, len: usize) {
     let mut i = 0;
     let mut j = len - 1;
-    
+
     while i < j {
         chars.swap(i, j);
-        i = i + 1;
-        j = j - 1;
+        i += 1;
+        j -= 1;
     }
-    
-    }
-    
-    use std::io::stdin;
-    use std::time::Instant;
-    
-    fn main() {
-    
+}
+
+use std::io::stdin;
+use std::time::Instant;
+
+fn main() {
     let mut input_string = String::new();
     stdin().read_line(&mut input_string)
-    .ok()
-    .expect("");
-    
+        .ok()
+        .expect("");
+
     let len = input_string.len();
     let mut input_string: Vec<_> = input_string.chars().collect();
-    
+
     let start = Instant::now();
     for _i in 0..100000000 {
         reverse(&mut input_string, len);
     }
     let duration = start.elapsed();
-    
-    println!("Time elapsed is: {:?}", duration);
-    
-    }
 
+    println!("Time elapsed is: {:?}", duration);
+}
